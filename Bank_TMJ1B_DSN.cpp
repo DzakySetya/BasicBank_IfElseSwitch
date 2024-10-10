3a6251d2a67d09ba642b6763b54b84ba0093919c
Dzaky Setya N <86037150+DzakySetya@users.noreply.github.com>
2024-10-10 10:53:56 +0700

    new elseif and rename Bank_TMJ1B.cpp to Bank_TMJ1B_DSN.cpp
    statment for limit

---
+++ Bank_TMJ1B_DSN.cpp
@@ -0,0 +1,69 @@
+// Dzaky Setya Nugraha (2407421047) TMJ1B 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int saldo, no_kegiatan;
+    int wd, bts_saldo, sisa_saldo_wd;
+    int depo, saldo_depo;
+    cout << "Selamat Datang di PNJ BANK\n" << endl;//judul
+
+    cout << "Masukkan Saldo : Rp";
+    cin >> saldo;
+
+    cout << "\nPilih Kegiatan yang Dipilih\n"
+        << "1. Penarikan Uang (Withdrawal)\n2. Penyetoran Uang (Deposit)\n3. Pemeriksaan Saldo (Check Balance)\n"
+        << "Masukkan Nomor Kegiatan : ";
+    cin >> no_kegiatan;
+    cout << "\n";
+
+    switch (no_kegiatan) {
+    case 1://withdraw
+        bts_saldo = saldo - 100000;
+
+        cout << "Masukkan uang yang anda ingin ambil :Rp";
+        cin >> wd;
+        sisa_saldo_wd = saldo - wd;
+
+        if (wd > 5000000) {
+            cout << "\nBatas Saldo yang Diambil Hanya Boleh Rp5.000.000!";
+        }
+        else if (wd <= 5000000 && wd <= bts_saldo && sisa_saldo_wd < 500000) {
+            sisa_saldo_wd -= 50000;
+            cout << "\nSaldo anda akan dikurangi Rp50.000 untuk biaya admin!";
+            cout << "\n\nSilahkan Ambil Duit Anda Sebesar :Rp" << wd;
+
+            cout << "\n\nSaldo Akhir : Rp" << sisa_saldo_wd;
+        }
+        else if (wd <= 5000000 && wd <= bts_saldo) {
+
+            cout << "\nSilahkan Ambil Duit Anda Sebesar :Rp" << wd;
+            cout << "\n\nSaldo Akhir : Rp" << sisa_saldo_wd;
+        }
+        else {
+            cout << "\nSaldo tidak boleh tersisa Rp100.000";
+        }
+        break;
+
+    case 2://Deposit
+        cout << "Masukkan duit yang ingin anda Deposit : ";
+        cin >> depo;
+
+        saldo_depo = saldo + depo;
+
+        cout << "\nSaldo Anda Sekarang : Rp" << saldo_depo;
+        break;
+
+    case 3://Check Balance
+        cout << "Saldo Anda Saat Ini : Rp" << saldo;
+        break;
+
+    default:
+        cout << "ERROR!";
+        break;
+    }
+
+}
